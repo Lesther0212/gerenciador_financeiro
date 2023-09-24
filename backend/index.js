77ef8235e9d2a92a045fe2564f9adc1b0899d9ac
Lesther0212 <murilodrumondlesther@gmail.com>
2023-09-24 19:15:58 -0300

    TestePessoa CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,114 +1,97 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql2/promise');
+const porta = 3000;
 
-app.use(express.json())
+const connection = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    database: 'testepessoa',
+    user: 'root',
+    password: ''
+});
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000,()=> console.log("Gatinho"));
+app.get('/pessoa', async (req, res) => {
+    try {
+        const [query] = await connection.execute('SELECT * FROM pessoa');
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao buscar pessoas' });
+    }
+});
 
-// app.get('/', (req,res)=>{
-    // res.send("minhal")
-// })
+app.get('/pessoa/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [query] = await connection.execute('SELECT * FROM pessoa WHERE id = ?', [id]);
+        if (query.length === 0) return res.status(404).json({ mensagem: 'Pessoa não encontrada' });
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao buscar pessoa' });
+    }
+});
 
-// app.get('/cachorro', (req,res)=>{
-    // res.send("AUAU")
-// })
+app.get('/pessoa/buscarnome/:nome', async (req, res) => {
+    const { nome } = req.params;
+    try {
+        const [query] = await connection.execute('SELECT * FROM pessoa WHERE nome LIKE ?', [`%${nome}%`]);
+        if (query.length === 0) return res.status(404).json({ mensagem: 'Nenhuma pessoa encontrada' });
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao buscar pessoas por nome' });
+    }
+});
 
-// app.get('/fim', (req,res)=> {res.end()} )
+app.get('/pessoa/buscaremail/:email', async (req, res) => {
+    const { email } = req.params;
+    try {
+        const [query] = await connection.execute('SELECT * FROM pessoa WHERE email LIKE ?', [`%${email}%`]);
+        if (query.length === 0) return res.status(404).json({ mensagem: 'Nenhuma pessoa encontrada' });
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao buscar pessoas por email' });
+    }
+});
 
-// const dados = ['Murilo'];
+app.post('/pessoa', async (req, res) => {
+    const { nome, email } = req.body;
+    try {
+        const [query] = await connection.execute('INSERT INTO pessoa (nome, email) VALUES (?, ?)', [nome, email]);
+        res.status(201).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao criar pessoa' });
+    }
+});
 
-// app.get('/j', (req,res)=>{res.json({dados})})
+app.put('/pessoa/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome, email } = req.body;
+    try {
+        const [query] = await connection.execute('UPDATE pessoa SET nome = ?, email = ? WHERE id = ?', [nome, email, id]);
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao atualizar pessoa' });
+    }
+});
 
-const mysql = require ('mysql2/promise');
-// const { Connection } = require('mysql2/typings/mysql/lib/Connection');
-const conection = mysql.createPool({
-    host:'localhost',
-    port:3306,
-    database:'testepessoa',
-    user:'root',
-    password:''
-})
+app.delete('/pessoa/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [query] = await connection.execute('DELETE FROM pessoa WHERE id = ?', [id]);
+        res.status(200).json(query);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao deletar pessoa' });
+    }
+});
 
-const getAllPessoas = async () => {
-    const [query] = await conection.execute('select * from pessoa')
-    return query
-}
-
-app.get('/pessoa', async (req,res)=>{
-    const resultado = await getAllPessoas()
-    return res.status(200).json(resultado)
-})
-
-app.get('/pessoa/:id', async (req,res) => {
-    const {id} = req.params;
-    const [query] = await conection.execute('select * from pessoa where id = ?'[id]);
-    if (query.length===0) return res.status(400).json({mensagem: "nenhuma pessoa encontrada"})
-    return res.status(200).json(query);
-})
-
-app.get('/pessoa/buscarnome/:nome', async (req,res)=>{
-    const {nome} = req.params;
-    const [query] = await conection.execute('select * from pessoa where nome like ?', ['%'+nome+'%']);
-    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-    return res.status(200).json(query)
-​
-}) 
-app.get('/pessoa/buscaremail/:email', async (req,res)=>{
-    const {email} = req.params;
-    const [query] = await conection.execute('select * from pessoa where email like ?', ['%'+email+'%']);
-    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-    return res.status(200).json(query)
-​
-})
-
-app.post('/pessoa', async (req,res)=>{
-    const {nome, email} = req.body;
-    const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)',[nome, email]);
-    return res.json(query);
-})
-app.put('/pessoa/:id', async (req,res)=>{
-    const {id} = req.params;
-    const {nome, email} = req.body;
-​
-    const [query] = await conection
-    .execute('update pessoa set nome = ?, email = ? where id = ?',[nome, email, id]);
-    return res.json(query);
-})
-​
-app.get('/pessoa/buscarnome/:nome', async (req,res)=>{
-    const {nome} = req.params;
-    const [query] = await conection.execute('select * from pessoa where nome like ?', ['%'+nome+'%']);
-    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-    return res.status(200).json(query)
-​
-}) 
-app.get('/pessoa/buscaremail/:email', async (req,res)=>{
-    const {email} = req.params;
-    const [query] = await conection.execute('select * from pessoa where email like ?', ['%'+email+'%']);
-    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
-    return res.status(200).json(query)
-​
-}) 
-​
-app.post('/pessoa', async (req,res)=>{
-    const {nome, email} = req.body;
-    const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)',[nome, email]);
-    return res.json(query);
-})
-app.put('/pessoa/:id', async (req,res)=>{
-    const {id} = req.params;
-    const {nome, email} = req.body;
-​
-    const [query] = await conection
-    .execute('update pessoa set nome = ?, email = ? where id = ?',[nome, email, id]);
-    return res.json(query);
-})
-​
-app.delete('/pessoa/:id', async(req,res) =>{
-    const {id} = req.params
-    const [query] = await conection.execute('delete from pessoa where id = ?', [id])
-    return res.json(query)
-})
+app.listen(porta, () => console.log(`Servidor está rodando na porta ${porta}`));
